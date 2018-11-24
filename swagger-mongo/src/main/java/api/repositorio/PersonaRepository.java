a5f9c8c4701f374b33f77b694cd98bd69c77d617
santis13 <santisanudo@gmail.com>
2018-11-25 00:09:10 +0100

    17

--- swagger-mongo/src/main/java/api/repositorio/PersonaRepository.java
+++ swagger-mongo/src/main/java/api/repositorio/PersonaRepository.java
@@ -14,7 +14,5 @@ public interface PersonaRepository extends MongoRepository<Persona, String> {
 	List<Persona> findByNombre(@Param("nombre") String nombre);
 	List<Persona> findByApellido1(@Param("apellido1") String apellido1);
 	List<Persona> findByApellido2(@Param("apellido2") String apellido2);
-	
-
-
+	List<Persona> deleteByDni(@Param("dni") String dni);
 }
